8addc550e86727c62cb71e8013a1601f139bd9da
SOgz12Z3Ce <sogz12z3ce@gmail.com>
2026-08-05 23:24:44 +0800

    feat: `diff()` returns entries now

--- src/thunderskin/package/diff.py
+++ src/thunderskin/package/diff.py
@@ -1,10 +1,10 @@
 from thunderskin.object import Object
 from thunderskin.package import Package
-from thunderskin.json.types import JsonObject
+from thunderskin.resource import Resource
 from thunderskin.exceptions import UnreachableError
 
 
-def diff(old: Package, new: Package) -> JsonObject:
+def diff(old: Package, new: Package) -> dict[str, list[Object | Resource]]:
     old_symbol_entry_map = {obj.symbol(): obj for obj in old.objects} | {
         res.symbol(): res for res in old.resources
     }
@@ -13,19 +13,23 @@ def diff(old: Package, new: Package) -> JsonObject:
     }
     old_symbols = set(old_symbol_entry_map.keys())
     new_symbols = set(new_symbol_entry_map.keys())
-    deleted_symbols = list(old_symbols - new_symbols)
-    newed_symbols = list(new_symbols - old_symbols)
+    deleted_symbols = old_symbols - new_symbols
+    newed_symbols = new_symbols - old_symbols
     common_symbols = old_symbols & new_symbols
-    modified_symbols = [
+    modified_symbols = {
         uid
         for uid in common_symbols
         if old_symbol_entry_map[uid] != new_symbol_entry_map[uid]
-    ]
-
-    return {
-        "new": sorted(newed_symbols + modified_symbols),
-        "delete": sorted(deleted_symbols),
     }
 
-def diff_list(packages: list[Package]) -> JsonObject:
+    new_objs = [new_symbol_entry_map[uid] for uid in (newed_symbols | modified_symbols)]
+    deleted_objs = [old_symbol_entry_map[uid] for uid in deleted_symbols]
+
+    return {
+        "new": sorted(new_objs, key=lambda x: x.symbol()),
+        "delete": sorted(deleted_objs, key=lambda x: x.symbol()),
+    }
+
+
+def diff_list(packages: list[Package]) -> list[dict[str, list[Object | Resource]]]:
     return [diff(pkg_old, pkg_new) for pkg_old, pkg_new in zip(packages, packages[1:])]
